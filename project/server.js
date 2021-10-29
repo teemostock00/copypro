93eb462e0f726daf92580cc9f3c72896b600bff2
heyjin281129 <kimhyejin281129@gmail.com>
2021-10-29 14:34:27 +0900

    g

--- project/server.js
+++ project/server.js
@@ -4,6 +4,8 @@ var express = require('express')
 // express 는 함수이므로, 반환값을 변수에 저장한다.
 var app = express()
 
+var router = require('./router/index')(app);
+
 // 3000 포트로 서버 오픈
 app.listen(3000, function() {
     console.log("start! express server on port 3000")
@@ -18,16 +20,3 @@ app.get('/', function(req,res) {
     res.sendFile(__dirname + "/public/view/index.html")
 })
 
-// localhost:3000/main 브라우저에 res.sendFile() 내부의 파일이 띄워진다.
-app.get('/index', function(req,res) {
-    res.sendFile(__dirname + "/public/view/index.html")
-})
-
-app.get('/introducation', function(req,res) {
-    res.sendFile(__dirname + "/public/view/introducation.html")
-})
-
-
-// public 디렉토리를 static으로 기억한다.
-// public 내부의 파일들을 localhost:3000/파일명 으로 브라우저에서 불러올 수 있다.
-app.use(express.static('public'))
