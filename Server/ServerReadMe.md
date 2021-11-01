a949080f787703cee91c6fd67439b29741dc32f5
teemostock00 <teemostock00@gmail.com>
2021-11-02 05:33:55 +0900

    서버에 템플릿엔진 써야될듯?

---
+++ Server/ServerReadMe.md
@@ -0,0 +1,10 @@
+package.json 파일 들어가서 모듈 확인해보고 server 경로의 터미널 열고나서 
+<br> ![img.png](img.png) 
+<br> start에 보면 app.js를 실행시키는 문장이 있음, 터미널에 yarn start나 npm start를 하면 이걸 실행시키는 거임
+<br> localhost:5000으로 홈페이지 들어가면 서버랑 같이 돌아가고 있음을 알 수 있음
+<br> <h3>핸들러</h3>
+server js에서 views 폴더에 layouts하고 partials로 구분지어 놓음 
+<br> layouts에서는 모든페이지에 항상 있는 부분을 지정
+<br> partials 폴더에는 자주쓰는 html 문구를 모아둬야함
+![img_1.png](img_1.png)
+
--- Server/app.js
+++ Server/app.js
@@ -1,18 +1,37 @@
-const express = require("express")
-const mysql = require("mysql")
+const express = require("express");
+const path = require('path');
+const mysql = require("mysql");
 const dotenv = require('dotenv');
+const hbs = require('express-handlebars');
 
 dotenv.config({ path: './.env'})
 
 const app = express();
 
 // mysql 연결
+// .env에 보안관련된거 싹다 넣고 git에 올리면 됨
 const db = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE
 });
+
+// 현재 디렉토리 엑세스 (콘솔에 띄워보면 경로뜸)
+// public 폴더에 js와 css 공용으로 사용할 수 있음
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static)
+// console.log(__dirname);
+
+// 핸들바
+app.engine( 'hbs', hbs( {
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts/',
+    partialsDir: __dirname + '/views/partials/'
+} ) );
+
+app.set('view engine', 'hbs');
 
 // 연결했을때 에러뜨면 콘솔에 띄워야 에러가 확인됨
 db.connect( (error) => {
@@ -26,7 +45,8 @@ db.connect( (error) => {
 
 // html get방식으로 가져오기
 app.get("/", (req, res) => {
-    res.send("<h1>Home page</h1>")
+    // res.send("<h1>Home page</h1>")
+    res.render("index")
 });
 
 app.listen(5000, () => {
